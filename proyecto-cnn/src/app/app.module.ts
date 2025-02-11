fe154e88e81e2aca52e798a09ee7b433875e0dab
dubclubargentina <sodeopolis@gmail.com>
2025-02-11 17:55:25 -0300

    app.module.ts

--- proyecto-cnn/src/app/app.module.ts
+++ proyecto-cnn/src/app/app.module.ts
@@ -1,36 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopheadingComponent } from './topheading/topheading.component';
-
-//Utsav Modules
-import {HttpClientModule} from '@angular/common/http';
-import {NewsapiservicesService} from './service/newsapiservices.service';
-import { TechnewsComponent } from './technews/technews.component';
-import { BusinessnewsComponent } from './businessnews/businessnews.component';
-import { EntertainmentnewsComponent } from './entertainmentnews/entertainmentnews.component';
-import { SportsnewsComponent } from './sportsnews/sportsnews.component';
-import { HealthnewsComponent } from './healthnews/healthnews.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopheadingComponent,
-    TechnewsComponent,
-    BusinessnewsComponent,
-    EntertainmentnewsComponent,
-    SportsnewsComponent,
-    HealthnewsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-
+    BrowserModule
   ],
-  providers: [NewsapiservicesService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
